d3410db50182f883f5a36054f5826cfa246eae9c
Roman <romaha48915@gmail.com>
2020-11-25 21:43:52 +0200

    add firebase package

---
+++ packages/authentication_repository/lib/src/models/user.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+/// User model
+
+class User extends Equatable {
+  const User({
+    @required this.email,
+    @required this.id,
+    @required this.name,
+    @required this.photo,
+  })  : assert(email != null),
+        assert(id != null);
+
+  final String email;
+
+  final String id;
+
+  final String name;
+
+  final String photo;
+
+  static const empty = User(email: '', id: '', name: null, photo: null);
+
+  @override
+  List<Object> get props => [email, id, name, photo];
+}
